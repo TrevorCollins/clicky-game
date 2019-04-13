818dc8a8591d90a0aea2ed404c3eac458eb1ed2b
TrevorCollins <tcollinst1i@gmail.com>
2019-04-13 14:26:46 -0500

    Updates

---
+++ precache-manifest.3f390dc4ced00c376ef0476aaf079b54.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "a35fbe7bcbdb2d8852e5",
+    "url": "/clicky-game/static/css/main.fa8c7e05.chunk.css"
+  },
+  {
+    "revision": "a35fbe7bcbdb2d8852e5",
+    "url": "/clicky-game/static/js/main.fcc284ff.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "8c3f2ee9c00af5a72a94",
+    "url": "/clicky-game/static/js/2.cc481a00.chunk.js"
+  },
+  {
+    "revision": "a4b00d5b4d878d346226bb823b230fa5",
+    "url": "/clicky-game/static/media/arya.a4b00d5b.jpg"
+  },
+  {
+    "revision": "25ec6355bccb8da2cb72ff3c2e4e8acf",
+    "url": "/clicky-game/static/media/bran.25ec6355.jpg"
+  },
+  {
+    "revision": "205a4cced7bee13aa40cdff22b5e4ec6",
+    "url": "/clicky-game/static/media/brienne.205a4cce.jpg"
+  },
+  {
+    "revision": "1fd813bb97cfb36b3404592542c93e36",
+    "url": "/clicky-game/static/media/bronn.1fd813bb.jpg"
+  },
+  {
+    "revision": "094ac1471cc491eaf975651f855df820",
+    "url": "/clicky-game/static/media/cerci.094ac147.jpg"
+  },
+  {
+    "revision": "d044510487713f32fc69c0eef4186001",
+    "url": "/clicky-game/static/media/daenerys.d0445104.jpg"
+  },
+  {
+    "revision": "9354b6b8c3851a3a0146d944d98a4d60",
+    "url": "/clicky-game/static/media/davos.9354b6b8.jpg"
+  },
+  {
+    "revision": "7c247be7c73d0ce737bde19f2f1050bd",
+    "url": "/clicky-game/static/media/gendry.7c247be7.jpg"
+  },
+  {
+    "revision": "8537eb849fd6f3edc9f3ca9be1c1ca47",
+    "url": "/clicky-game/static/media/jaime.8537eb84.jpg"
+  },
+  {
+    "revision": "888ce090f9b6b4168ff6839b6829cc2b",
+    "url": "/clicky-game/static/media/jon.888ce090.jpg"
+  },
+  {
+    "revision": "57d9863f9777ea8bb1dfa616c915cb4b",
+    "url": "/clicky-game/static/media/jorah.57d9863f.jpg"
+  },
+  {
+    "revision": "7a976e5f85a9a9b35ccd2a7e606592ce",
+    "url": "/clicky-game/static/media/sam.7a976e5f.jpg"
+  },
+  {
+    "revision": "c6f62e980e549e262ceaaf00c6fdc8ad",
+    "url": "/clicky-game/static/media/sandor.c6f62e98.jpg"
+  },
+  {
+    "revision": "4664a5d8dfcc1b24119a694f6a3dcdbc",
+    "url": "/clicky-game/static/media/sansa.4664a5d8.jpg"
+  },
+  {
+    "revision": "914935523117309debf35bd23c30e187",
+    "url": "/clicky-game/static/media/theon.91493552.jpg"
+  },
+  {
+    "revision": "43427a093617ba88a47d83305b1596fc",
+    "url": "/clicky-game/static/media/tormund.43427a09.jpg"
+  },
+  {
+    "revision": "01ed5bff501ead0d36f1aa69009f724e",
+    "url": "/clicky-game/static/media/tyrion.01ed5bff.jpg"
+  },
+  {
+    "revision": "41497cb687eaf26a0e3f0e93968e8383",
+    "url": "/clicky-game/static/media/varys.41497cb6.jpg"
+  },
+  {
+    "revision": "bc0b22694d50464fb8bbd6e49863c1c9",
+    "url": "/clicky-game/index.html"
+  }
+];
